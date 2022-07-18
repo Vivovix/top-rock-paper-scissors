82c28964907977a50a592a7de00c7350d98193e4
Vivovix <noudjemaas@gmail.com>
2022-07-18 21:47:03 +0200

    Add game logic

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 "use strict";
 
-let computerSelection;
-let playerSelection;
-
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 100);
     if (x == 0) {
         return getComputerChoice();
     }
     if (x < 33) {
-        return "Rock";
-    } if (x > 32 && x < 67) {
-        return "Paper";
-    } if (x > 67) {
-        return "Scissors"
+        return "rock";
+    }
+    if (x > 32 && x < 67) {
+        return "paper";
+    }
+    if (x > 67) {
+        return "scissors"
     } 
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection == computerSelection) {
+        return `It's a draw, you both picked ${playerSelection}. Try again!`;
+    } if (playerSelection == "rock") {
+        return computerSelection == "paper" ? "Paper beats rock. You lose! :("
+        : "Rock beats scissors. You win! :)";
+    } if (playerSelection == "paper") {
+        return computerSelection == "scissors" ? "Scissors beat paper. You lose! :("
+        : "Paper beats rock. You win! :)"
+    } if (playerSelection == "scissors") {
+        return computerSelection == "rock" ? "Rock beats scissors. You lose! :("
+        : "Scissors beat paper. You win! :)"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
