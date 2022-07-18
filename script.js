d411ced964d2b151753d8f7d79f5d1c5d130847f
Vivovix <noudjemaas@gmail.com>
2022-07-18 22:33:28 +0200

    Add player prompt. Add helper functions.

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
 "use strict";
 
 function getComputerChoice() {
-    let x = Math.floor(Math.random() * 100);
-    if (x == 0) {
+    let random = Math.floor(Math.random() * 100);
+    console.log(random);
+    if (random == 0) {
         return getComputerChoice();
     }
-    if (x <= 32) {
+    if (random <= 32) {
         return "rock";
     }
-    if (x > 32 && x < 67) {
+    if (random > 32 && random < 67) {
         return "paper";
     }
-    if (x >= 67) {
+    if (random >= 67) {
         return "scissors"
     } 
 }
 
+function getPlayerChoice() {
+    let choice = prompt("What will you pick?")
+    if (choice == null) { //fix player pressing cancel
+        alert("No good! Only 'Rock', 'Paper' and 'Scissors' allowed!");
+        return getPlayerChoice();
+    } else {
+        choice = choice.toLowerCase();
+    }
+    if (choice != "rock" && choice != "paper" && choice != "scissors") {
+        alert("No good! Only 'Rock', 'Paper' and 'Scissors' allowed!");
+        return getPlayerChoice();
+    } return choice;
+}
+
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection);
-    console.log(computerSelection);
     if (playerSelection == computerSelection) {
-        return `It's a draw, you both picked ${playerSelection}. Try again!`;
+        return 0;
     } if (playerSelection == "rock") {
-        return computerSelection == "paper" ? "Paper beats rock. You lose! :("
-        : "Rock beats scissors. You win! :)";
+        return computerSelection == "paper" ? false
+        : true;
     } if (playerSelection == "paper") {
-        return computerSelection == "scissors" ? "Scissors beat paper. You lose! :("
-        : "Paper beats rock. You win! :)"
+        return computerSelection == "scissors" ? false
+        : true
     } if (playerSelection == "scissors") {
-        return computerSelection == "rock" ? "Rock beats scissors. You lose! :("
-        : "Scissors beat paper. You win! :)"
+        return computerSelection == "rock" ? false
+        : true
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        console.log(`You picked ${playerSelection}. The computer picked ${computerSelection}.`);
+        if (playRound(playerSelection, computerSelection) == 0) {
+            console.log(`You both picked ${playerSelection}! No winner!`)
+        } else if (playRound(playerSelection, computerSelection) == true) {
+            console.log("You win!");
+            playerScore++;
+        } else {
+            console.log("You lose!");
+            computerScore++;
+        }
+    }
+    console.log(`You won ${playerScore} rounds. The computer won ${computerScore} rounds.`);
+}
+
+
+
+game();
